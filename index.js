426811ffd8ca5ef33994559f14de51a4596425d3
Johnnick F. Landim <78829381+johnnickjf@users.noreply.github.com>
2023-05-19 20:38:27 -0300

    create: Estrutura base pronta.

--- index.js
+++
@@ -1,9 +0,0 @@
-const GameTree = require('./Game');
-const Search = require('./Search');
-
-const primeiroTabuleiro = ['', '', '', '', 'X', '', '', '', '']
-const tabuleiroPreenchido = new GameTree(primeiroTabuleiro);
-// console.log(tabuleiroPreenchido);
-const melhorJogada = Search.melhorEscolha(tabuleiroPreenchido.raiz, 'O');
-
-console.log(melhorJogada);
